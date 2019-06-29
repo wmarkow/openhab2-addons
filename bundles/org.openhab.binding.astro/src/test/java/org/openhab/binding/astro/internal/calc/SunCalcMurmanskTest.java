972dcac6f0db61c2f3f220ca53d4ba90c42bc6dd
Witold Markowski <witold.a.markowski@gmail.com>
2019-06-29 18:37:17 +0100

    Another test for Murmansk.
    Signed-off-by: Witold Markowski <witold.a.markowski@gmail.com>

--- bundles/org.openhab.binding.astro/src/test/java/org/openhab/binding/astro/internal/calc/SunCalcMurmanskTest.java
+++ bundles/org.openhab.binding.astro/src/test/java/org/openhab/binding/astro/internal/calc/SunCalcMurmanskTest.java
@@ -102,7 +102,7 @@ public class SunCalcMurmanskTest extends AbstractSunCalcLocationTest {
 
         assertNull(sun.getNight().getStart());
         assertNull(sun.getNight().getEnd());
-        
+
         assertEquals(TestUtils.newCalendar(2019, Calendar.JUNE, 21, 0, 49, MOSCOW_TIME_ZONE).getTimeInMillis(),
                 sun.getTrueMidnight().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
     }
@@ -111,5 +111,75 @@ public class SunCalcMurmanskTest extends AbstractSunCalcLocationTest {
     public void testRangesCoherenceFor_2019_June_21_at_midnight() {
         testRangesCoherence(new GregorianCalendar(2019, Calendar.JUNE, 21, 0, 0), MURMANSK_LATITUDE,
                 MURMANSK_LONGITUDE);
+    }
+
+    @Test
+    public void testRangesFor_2019_May_21_at_midnight() {
+        Calendar date = new GregorianCalendar(2019, Calendar.MAY, 21, 0, 0);
+        date.setTimeZone(TimeZone.getTimeZone("Europe/Moscow"));
+
+        Sun sun = subject.getSunInfo(date, MURMANSK_LATITUDE, MURMANSK_LONGITUDE, 0.0);
+
+        assertNull(sun.getAstroDawn().getStart());
+        assertNull(sun.getAstroDawn().getEnd());
+
+        assertNull(sun.getNauticDawn().getStart());
+        assertNull(sun.getNauticDawn().getEnd());
+
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 0, 44, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getCivilDawn().getStart().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 1, 11, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getCivilDawn().getEnd().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 1, 11, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getRise().getStart().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+        // it looks like the rise duration is around 35 minutes here
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 1, 46, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getRise().getEnd().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 1, 46, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getDaylight().getStart().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+        // FIXME: Astro calculates a DAYLIGHT END here (however it is not true):
+        // probably it is because of a low solar accuracy formulas used in Astro or
+        // because in Astro DAYLIGHT end when Sun set starts
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 23, 46, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getDaylight().getEnd().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 12, 44, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getNoon().getStart().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 12, 45, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getNoon().getEnd().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+
+        // FIXME: Astro calculates a SUN_SET here (however it is not true):
+        // probably it is because of a low solar accuracy formulas used in Astro
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 23, 46, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getSet().getStart().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+        // it looks like the rise duration is around 34 minutes here
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 22, 0, 20, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getSet().getEnd().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+
+        // we are between MIDNIGHT and TRUE MIDNIGHT, show DUSK from 20th/21th
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 0, 0, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getCivilDusk().getStart().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 0, 44, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getCivilDusk().getEnd().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
+
+        assertNull(sun.getNauticDusk().getStart());
+        assertNull(sun.getNauticDusk().getEnd());
+
+        assertNull(sun.getAstroDusk().getStart());
+        assertNull(sun.getAstroDusk().getEnd());
+
+        assertNull(sun.getMorningNight().getStart());
+        assertNull(sun.getMorningNight().getEnd());
+
+        assertNull(sun.getEveningNight().getStart());
+        assertNull(sun.getEveningNight().getEnd());
+
+        assertNull(sun.getNight().getStart());
+        assertNull(sun.getNight().getEnd());
+
+        assertEquals(TestUtils.newCalendar(2019, Calendar.MAY, 21, 0, 44, MOSCOW_TIME_ZONE).getTimeInMillis(),
+                sun.getTrueMidnight().getTimeInMillis(), NOON_ACCURACY_IN_MILLIS);
     }
 }
